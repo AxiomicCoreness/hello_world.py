91904ad6326d2db2d1e5ce7de97b10c2e0592228
AxiomicCoreness <mrteanotcoffee@gmail.com>
2026-08-19 01:43:53 -0400

    feat: export dsh_adapter from deepseek_mesh quadrant

--- quantum/deepseek_mesh/__init__.py
+++ quantum/deepseek_mesh/__init__.py
@@ -1,6 +1,6 @@
-# DeepSeek Mesh Quadrant - Entry 8844/8845
-# DeepSeek client and /mesh/invoke endpoint
+# DeepSeek Mesh Quadrant - Entry 8844/8845 + harness lattice
+# DeepSeek client, /mesh endpoint, official-SDK soft adapter
 
-from . import client, endpoint
+from . import client, endpoint, dsh_adapter
 
-__all__ = ["client", "endpoint"]
+__all__ = ["client", "endpoint", "dsh_adapter"]
