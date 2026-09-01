8729aa173781d2de701f1fbc6b4fdb8c9b962f8a
AxiomicCoreness <mrteanotcoffee@gmail.com>
2026-09-01 13:06:08 -0400

    Add new imports to __init__.py for additional features
    Signed-off-by: AxiomicCoreness <mrteanotcoffee@gmail.com>

--- garden_surgery/__init__.py
+++ garden_surgery/__init__.py
@@ -8,6 +8,14 @@ Run:  PYTHONPATH=. python3 -m garden_surgery
 from garden_surgery.theorems import TheoremReport, check_theorems
 from garden_surgery.environment import EnvReport, probe_environment
 from garden_surgery.surfaces import SurfaceReport, probe_surfaces
+from garden_surgery.trigger_excavate import (
+    KAPPA_DECLARED,
+    diagnostic_scalars,
+    golden_hash,
+    kappa_decomposition,
+)
+from garden_surgery.anomaly_distance import payload as anomaly_payload, math_form, override_payload, response_headers
+from garden_surgery.worker_tree import tree_payload, node, parent_of, siblings_of, children_of, lineage
 
 __all__ = [
     "TheoremReport",
@@ -17,6 +25,20 @@ __all__ = [
     "probe_environment",
     "probe_surfaces",
     "diagnose",
+    "KAPPA_DECLARED",
+    "diagnostic_scalars",
+    "golden_hash",
+    "kappa_decomposition",
+    "anomaly_payload",
+    "math_form",
+    "override_payload",
+    "response_headers",
+    "tree_payload",
+    "node",
+    "parent_of",
+    "siblings_of",
+    "children_of",
+    "lineage",
 ]
 
 
