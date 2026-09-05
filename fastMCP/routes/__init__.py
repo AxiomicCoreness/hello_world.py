6e100e91788feb2696c4d0072bb985a7c1fcc93a
AxiomicCoreness <mrteanotcoffee@gmail.com>
2026-09-05 09:37:29 -0400

    Initialize routes package for fastMCP
    Signed-off-by: AxiomicCoreness <mrteanotcoffee@gmail.com>

---
+++ fastMCP/routes/__init__.py
@@ -0,0 +1,5 @@
+"""Routes package for fastMCP."""
+
+from fastMCP.routes.health import router as health_router
+from fastMCP.routes.status import router as status_router
+from fastMCP.routes.mcp import router as mcp_router
